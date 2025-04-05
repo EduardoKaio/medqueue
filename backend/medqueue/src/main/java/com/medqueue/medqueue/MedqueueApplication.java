0a24aaa6d4a493b659509b1b79fe86fe52fc11d4
Kaio Eduardo <kaioeduardooficial@gmail.com>
2025-04-05 14:27:25 -0300

    feat: add spring backend

---
+++ backend/medqueue/src/main/java/com/medqueue/medqueue/MedqueueApplication.java
@@ -0,0 +1,13 @@
+package com.medqueue.medqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedqueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedqueueApplication.class, args);
+	}
+
+}

6344eefc4cb962416a268d4271fb79697536a998
Felipe Freitas <felipefreitas13@live.com>
2025-07-15 15:16:10 -0300

    refactor: cor de fundo

--- frontend/vetqueue/src/pages/auth/Register.jsx
+++ frontend/vetqueue/src/pages/auth/Register.jsx
@@ -25,7 +25,7 @@ const Root = styled(Box)({
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
-  backgroundColor: "#388E3C",
+  backgroundColor: "#0079CD",
 });
 
 const PaperWrapper = styled(Paper)({
